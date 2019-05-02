4a0446fb8856353921b04de3b4c7d4b0a6928c2f
Michael Borglin <michael@team.casa>
2019-05-02 14:27:19 -0700

    fix bug when opening channels with btc preference

---
+++ components/Lightning/Alerts/Success.vue
@@ -0,0 +1,26 @@
+<template>
+  <form action="">
+      <div class="alert-icon-container">
+        <img src="~assets/green-check.svg" alt="Success">
+      </div>
+      <div class="alert alert-status modal-card">
+        <header class="modal-card-head"></header>
+        <section class="modal-card-body">
+          <h2>Success.</h2>
+          <p>Your changes have been saved.</p>
+        </section>
+        <footer class="modal-card-foot">
+          <a class="button is-casa is-fullwidth" @click="$parent.close()">Continue</a>
+        </footer>
+      </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Success',
+  data() {
+    return {}
+  }
+};
+</script>
